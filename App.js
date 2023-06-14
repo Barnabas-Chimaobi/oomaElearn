fa6b0bbe4be9e5ce660cb4f7e25de18433e523c7
Ah-ugo <ahuekweprinceugo@gmail.com>
2023-06-14 18:28:23 +0100

    Sign up OTP Screen

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import Login from './Screens/Login';
 import Login2 from './Screens/Login2';
 import LoginEror from './Screens/LoginError';
 import SignUp2 from './Screens/SignUp2';
+import SignUpOtp from './Screens/SignUpOtp';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <SignUp2/>
+      <SignUpOtp/>
       <StatusBar style="auto" />
     </View>
   );
